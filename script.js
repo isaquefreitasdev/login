f83362e391cf806efc39b6973cf1460bdff02fc4
Isaque <freitasmelo.mkt.online@gmail.com>
2024-11-02 16:43:49 -0300

    focus dos inputs

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
+const btn = document.querySelector("#btn")
+
+
+function emailFocus(){
+    const emailValue = email.value;
+    if(!emailValue){
+        email.classList.add("error")
+    }else{
+        email.classList.remove("error")
+    }
+}
+function passwordFocus(){
+    const passwordValue = password.value;
+    if(!passwordValue){
+        password.classList.add("error")
+    }else{
+        password.classList.remove("error")
+    }
+   
+}
+
+email.addEventListener("blur",emailFocus)
+password.addEventListener("blur",passwordFocus)
